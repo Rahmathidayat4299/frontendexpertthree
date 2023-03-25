f99e07e848cb100bf9edb3759da6f0b39f279613
Rahmathidayat4299 <hrahmat950@gmail.com>
2023-03-25 18:20:48 +0700

    add fitur workbox

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,27 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-import './component/app-bar';
-import './component/footer-bar';
+/* eslint-disable no-unused-vars */
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "./component/app-bar";
+import "./component/footer-bar";
 // import './component/hero-element';
-import App from './views/app';
+import App from "./views/app";
 // import main from './views/pages/main';
+import swRegister from "./utils/sw-register";
 
 // document.addEventListener('DOMContentLoaded', main);
 
 const app = new App({
-  button: document.querySelector('#hamburgerButton'),
-  drawer: document.querySelector('#navigationDrawer'),
-  content: document.querySelector('#posts'),
+  button: document.querySelector("#hamburgerButton"),
+  drawer: document.querySelector("#navigationDrawer"),
+  content: document.querySelector("#posts"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   app.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   app.renderPage();
+  swRegister();
   //   swRegister();
 });
